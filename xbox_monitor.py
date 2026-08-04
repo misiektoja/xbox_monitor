bc18e26b0b221d217be4034af3c666d3f825d398
misiektoja <misiektoja-github@rm-rf.ninja>
2026-08-04 02:11:56 +0200

    chore(release): bump runtime version to 1.9.2

--- xbox_monitor.py
+++ xbox_monitor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Author: Michal Szymanski <misiektoja-github@rm-rf.ninja>
-v1.9.1
+v1.9.2
 
 Tool implementing real-time tracking of Xbox Live players activities:
 https://github.com/misiektoja/xbox_monitor/
@@ -17,7 +17,7 @@ tzlocal (optional)
 python-dotenv (optional)
 """
 
-VERSION = "1.9.1"
+VERSION = "1.9.2"
 
 # ---------------------------
 # CONFIGURATION SECTION START
